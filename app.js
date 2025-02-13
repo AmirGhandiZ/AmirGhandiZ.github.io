cdd230b2063e78ad131c4ae4816c77e06b8c8026
AmirGhandiZ <160327140+AmirGhandiZ@users.noreply.github.com>
2025-02-13 22:43:12 +0330

    added music and lyrics

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const envelope = document.querySelector(".envelope-wrapper");
 envelope.addEventListener("click", () => {
   envelope.classList.toggle("flap");
 });
+
+let audio = document.querySelector(".music-file");
+audio.volume = 0.5;
